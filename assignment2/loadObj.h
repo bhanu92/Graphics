77cb0cd43a5147d43b004526cc1b9d22678b09f9
Bhanu <bhanu92@email.arizona.edu>
2017-03-03 10:34:06 -0700

    Done with scene file parsing

--- assignment2/loadObj.h
+++ assignment2/loadObj.h
@@ -15,75 +15,101 @@ using namespace glm;
 
 #include <iostream>
 #include <vector>
-#include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 
 typedef struct {
-	char name[32];	// identifier, i.e. "red"
-	vec3 ambient;	// ambient color, r,g,b
-	vec3 diffuse;	// diffuse color, r,g,b
-	vec3 specular;	// specular color, r,g,b
-	float shininess;	// specular power
-	char diffuseTexMap[128];	// file name of diffuse color texture map
-	char bumpTexMap[128];		// file name of bump map
+        char name[32];  // identifier, i.e. "red"
+        vec3 ambient;   // ambient color, r,g,b
+        vec3 diffuse;   // diffuse color, r,g,b
+        vec3 specular;  // specular color, r,g,b
+        float shininess;        // specular power
+        char diffuseTexMap[128];        // file name of diffuse color texture map
+        char bumpTexMap[128];           // file name of bump map
 } colorValues;
 
 class three {
 public:
-	// constructor with three parameters
-	three(float f1, float f2, float f3) {
-		x = f1;
-		y = f2;
-		z = f3;
-	}
-	// constructor with no parameters 
-	three() {};
-	// member data
-	float x;
-	float y;
-	float z;
+// constructor with three parameters
+three(float f1, float f2, float f3) {
+        x = f1;
+        y = f2;
+        z = f3;
+}
+// constructor with no parameters
+three() {
+};
+// member data
+float x;
+float y;
+float z;
 };
 
 
+// View description
+typedef struct {
+        vec3 eye;
+        vec3 center;
+        vec3 viewup;
+}viewProperties;
+
+// Object description
+typedef struct {
+        string objPath;
+        string shaderType;
+        vec3 t;
+        float rx;
+        vec3 s;
+        mat4 modelMatrix;
+} objectProperties;
+
 // light description
 typedef struct {
-	int isEnabled;	// is this light active
-	int isLocal;	// true for point light source, false for directional light
-	int isSpot;		// true for spotlight
-	vec3 ambient;	// light's contribution to ambient light (r,g,b)
-	vec3 color;		// color of light (r,g,b)
-	vec3 position;	// location of local light source, direction to light source otherwise
-	vec3 halfVector;
-	vec3 coneDirection;		// spotlight attributes
-	float spotCosCutoff;
-	float spotExponent;
-	float constantAttenuation;	// local light attenuation coefficients
-	float linearAttenuation;
-	float quadraticAttenuation;
+        int isEnabled;  // is this light active
+        int isLocal;    // true for point light source, false for directional light
+        int isSpot;             // true for spotlight
+        vec3 ambient;   // light's contribution to ambient light (r,g,b)
+        vec3 color;             // color of light (r,g,b)
+        vec3 position;  // location of local light source, direction to light source otherwise
+        vec3 halfVector;
+        vec3 coneDirection;             // spotlight attributes
+        float spotCosCutoff;
+        float spotExponent;
+        float constantAttenuation;      // local light attenuation coefficients
+        float linearAttenuation;
+        float quadraticAttenuation;
 }lightProperties;
+
+
+// Structure for the entire scene information
+typedef struct {
+        viewProperties view;
+        vector<lightProperties> light;
+        vector<objectProperties> object;
+}sceneInfo;
+
 
 typedef enum { BLINN_PHONG, COOK_TORRANCE, TOON } shaderType;
 
 typedef struct {
-	// geometric information
-	GLuint VAO;				// vertex array object for this object
-	GLuint VAOsize;			// number of primitives (triangles) for this object
-	float bounds[6];		// min,max X, Y, Z for this object
-	mat4 modelMatrix;		// composite modeling matrix
-	// lighting information
-	shaderType shader;
-	// material information, e.g. lighting parameters and texture maps
-	char materialName[32];	// identifier of this entry in the material file
-	vec3 Ka;				// ambient color for this object
-	vec3 Kd;				// diffuse color for this object
-	vec3 Ks;				// specular color for this object
-	float n;				// exponent for dot(N,H) or shininess
-	char diffuseTex[128];	// file name of diffuse texture map
-	int diffuseTexMapID;	// texture unit ID
-	char bumpMap[128];		// file name of bump map
-	int bumpMapTexID;		// texture unit ID
+        // geometric information
+        GLuint VAO;                             // vertex array object for this object
+        GLuint VAOsize;                 // number of primitives (triangles) for this object
+        float bounds[6];                // min,max X, Y, Z for this object
+        mat4 modelMatrix;               // composite modeling matrix
+        // lighting information
+        shaderType shader;
+        // material information, e.g. lighting parameters and texture maps
+        char materialName[32];  // identifier of this entry in the material file
+        vec3 Ka;                                // ambient color for this object
+        vec3 Kd;                                // diffuse color for this object
+        vec3 Ks;                                // specular color for this object
+        float n;                                // exponent for dot(N,H) or shininess
+        char diffuseTex[128];   // file name of diffuse texture map
+        int diffuseTexMapID;    // texture unit ID
+        char bumpMap[128];              // file name of bump map
+        int bumpMapTexID;               // texture unit ID
 } objInfo;
 
 // prototype for loadObjFile
@@ -93,4 +119,6 @@ int loadObjFile(char *fileName, objInfo **theObjects, int *numObjects);
 // out: the number of objInfo records in theObjects array
 // memory for theObjects array is allocated and managed within loadObjFile function
 
-#endif	//LOADOBJ_H
+sceneInfo sceneParse(char* file);
+
+#endif  //LOADOBJ_H
---
+++ assignment2/sceneParse.cpp
@@ -0,0 +1,161 @@
+// Code to parse the entire specified scene file
+// Reads the entire file word by word and stores into a string vector.
+
+#include <istream>
+#include <sstream>
+#include <algorithm>
+
+#include "loadObj.h"
+
+#define PI 3.14
+
+sceneInfo parseScene(char* sceneFile){
+
+        sceneInfo scene;
+        cout << "Scene File Name: " << endl;
+
+        vector<string> tokens;
+        string token;
+
+        ifstream file;
+        file.open("sceneFile");
+        if(!file.is_open()) {
+                cout << "Cannot open Scene file" << endl;
+                exit(EXIT_FAILURE);
+        }
+
+        while(file >> token) {
+                tokens.push_back(token);
+        }
+
+        // Adding NULL values at the end of the vector
+        // As the scenefiles are of varying size
+        for(int i = 0; i < 32; i++)
+                tokens.push_back('\0');
+
+        for (int i = 0; i < tokens.size(); i++) {
+
+                if(tokens.at(i).compare("view") == 0) {
+
+                        scene.view.eye.x =  stof(tokens.at(i+2));
+                        scene.view.eye.y =  stof(tokens.at(i+3));
+                        scene.view.eye.z =  stof(tokens.at(i+4));
+
+                        scene.view.center.x =  stof(tokens.at(i+6));
+                        scene.view.center.y =  stof(tokens.at(i+7));
+                        scene.view.center.z =  stof(tokens.at(i+8));
+
+                        scene.view.center.x =  stof(tokens.at(i+10));
+                        scene.view.center.y =  stof(tokens.at(i+11));
+                        scene.view.center.z =  stof(tokens.at(i+12));
+
+                        continue;
+
+                }
+
+                else if(tokens.at(i).compare("light") == 0) {
+
+                        lightProperties sceneLight;
+
+                        if(tokens.at(i+2).compare("directional") == 0)
+                                sceneLight.isLocal = 0;
+
+                        else if(tokens.at(i+2).compare("local") == 0)
+                                sceneLight.isLocal = 1;
+
+                        else if(tokens.at(i+2).compare("spot") == 0)
+                                sceneLight.isSpot = 1;
+                        sceneLight.isLocal = 1;
+
+                        if(tokens.at(i+3).compare("ambient") == 0) {
+                                sceneLight.ambient.x = stof(tokens.at(i+4));
+                                sceneLight.ambient.y = stof(tokens.at(i+5));
+                                sceneLight.ambient.z = stof(tokens.at(i+6));
+                        }
+
+                        if(tokens.at(i+7).compare("color") == 0) {
+                                sceneLight.color.x = stof(tokens.at(i+8));
+                                sceneLight.color.y = stof(tokens.at(i+9));
+                                sceneLight.color.z = stof(tokens.at(i+10));
+                        }
+
+                        if(tokens.at(i+11).compare("position") == 0) {
+                                sceneLight.position.x = stof(tokens.at(i+12));
+                                sceneLight.position.y = stof(tokens.at(i+13));
+                                sceneLight.position.z = stof(tokens.at(i+14));
+                        }
+
+                        if(tokens.at(i+15).compare("constAtt") == 0)
+                                sceneLight.constantAttenuation = stof(tokens.at(i+16));
+
+                        if(tokens.at(i+17).compare("linearAtt") == 0)
+                                sceneLight.linearAttenuation = stof(tokens.at(i+18));
+
+                        if(tokens.at(i+19).compare("quadAtt") == 0)
+                                sceneLight.quadraticAttenuation = stof(tokens.at(i+20));
+
+                        if(tokens.at(i+21).compare("coneDirection") == 0) {
+                                sceneLight.coneDirection.x = stof(tokens.at(i+22));
+                                sceneLight.coneDirection.y = stof(tokens.at(i+23));
+                                sceneLight.coneDirection.z = stof(tokens.at(i+24));
+                        }
+
+                        if(tokens.at(i+25).compare("spotCosCutoff") == 0)
+                                sceneLight.spotCosCutoff = stof(tokens.at(i+26));
+
+                        if(tokens.at(i+27).compare("spotExponent") == 0)
+                                sceneLight.spotExponent = stof(tokens.at(i+28));
+
+                        //End of reading one light information
+                        //Stuffing the above info into the light vector
+                        scene.light.push_back(sceneLight);
+
+                        continue;
+
+                }
+
+                else if(tokens.at(i).compare("object") == 0) {
+
+                        objectProperties sceneObject;
+                        mat4 modMat(1.0f);
+
+
+                        sceneObject.objPath = tokens.at(i+1);
+                        sceneObject.shaderType = tokens.at(i+3);
+
+                        //rotation
+                        if(tokens.at(i+4).compare("rx") == 0) {
+                                sceneObject.rx = stof(tokens.at(i+5));
+                                vec3 xaxis = {1, 0, 0};
+                                modMat = modMat * rotate(modMat, float((sceneObject.rx * PI)/180.0f), xaxis);
+                                sceneObject.modelMatrix = modMat;
+                        }
+
+                        //translation
+                        if(tokens.at(i+4).compare("t") == 0) {
+                                sceneObject.t.x = stof(tokens.at(i+5));
+                                sceneObject.t.y = stof(tokens.at(i+6));
+                                sceneObject.t.z = stof(tokens.at(i+7));
+                                modMat = modMat * translate(modMat, sceneObject.t);
+                                sceneObject.modelMatrix = modMat;
+                        }
+
+                        //scaling
+                        if(tokens.at(i+4).compare("s") == 0) {
+                                sceneObject.s.x = stof(tokens.at(i+5));
+                                sceneObject.s.y = stof(tokens.at(i+6));
+                                sceneObject.s.z = stof(tokens.at(i+7));
+                                modMat = modMat * scale(modMat, sceneObject.s);
+                                sceneObject.modelMatrix = modMat;
+                        }
+
+                        //End of reading one object description
+                        //Stuffing the above info into the object vector
+                        scene.object.push_back(sceneObject);
+
+                }
+
+        }
+
+        return scene;
+}
